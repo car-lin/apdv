674b7c2ca7bf3fd1b56ba4ebb08c97426aa10c73
Carline Imakulate Vincent Raj <105123085+car-lin@users.noreply.github.com>
2025-12-30 14:13:21 +0000

    Refactor load_historical_to_postgres function
    Refactored load_historical_to_postgres function to improve structure and readability.

--- load.py
+++ load.py
@@ -1,7 +1,7 @@
 from io import StringIO
 from sqlalchemy import create_engine, text
 
-def load_historical_to_postgres(df_csv, engine):
+'''def load_historical_to_postgres(df_csv, engine):
     print("   1.3 Bulk Insert to PostgreSQL...")
     with engine.connect() as conn:
         conn.execute(text("TRUNCATE TABLE historical_stations RESTART IDENTITY CASCADE"))
@@ -20,7 +20,31 @@ def load_historical_to_postgres(df_csv, engine):
         connection.commit()
     finally:
         connection.close()
-    print("The HISTORICAL CSV (2024-09-01) is successfully stored into POSTGRESQL")
+    print("The HISTORICAL CSV (2024-09-01) is successfully stored into POSTGRESQL")'''
+
+def load_historical_to_postgres(df_csv, engine):
+    print("   1.3 Bulk Insert to PostgreSQL...")
+
+    with engine.connect() as conn:
+        conn.execute(text("TRUNCATE TABLE historical_stations RESTART IDENTITY CASCADE"))
+        conn.commit()
+
+    columns_to_store = [
+        'station_id', 'name', 'capacity', 'lat', 'lon',
+        'last_reported', 'num_bikes_available', 'num_docks_available',
+        'utilization', 'imbalance', 'hour', 'weekday'
+    ]
+
+    df_csv[columns_to_store].to_sql(
+        "historical_stations",
+        engine,
+        if_exists="append",
+        index=False,
+        chunksize=1000,
+        method="multi"
+    )
+
+    print("Historical data successfully loaded")
 
 
 def load_realtime_to_postgres(df_realtime, engine):
